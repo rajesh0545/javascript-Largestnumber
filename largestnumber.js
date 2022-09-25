4e4e6582603325ed2c67aaedf1fdb1dd72ba0a64
rajesh <krajeshgowri7032@mail.com>
2022-09-25 18:32:39 +0530

    add files

---
+++ largestnumber.js
@@ -0,0 +1,35 @@
+// const num1=10;
+// const num2=20;
+// const num3=93;
+// let largest;
+
+// if(num1>=num2 && num1 >=num3){
+//     largest= num3;
+// }
+// else if(num2>=num1 && num2>=num3){
+//     largest=num3;
+// }
+//   else{
+//     largest=num3;
+//   }
+
+  // console.log(`the largest number is ${largest}`)
+
+const num1=20;
+const num2=30;
+const num3=60;
+
+
+if(num1>=num2 && num1>=num3){
+  console.log(`the largest number is ${num1}`)
+}
+
+else if(num2>=num1 && num2>=num3){
+  console.log(`the largest number is ${num2}`)
+
+
+}
+
+else{
+  console.log(`this number is largest ${num3}`);
+}
